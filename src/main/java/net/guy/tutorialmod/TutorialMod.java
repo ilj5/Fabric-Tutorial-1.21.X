a79339541a5245b838818ed88d14ec1d1a61bc8e
Guy <lasovskiguy@gmail.com>
2025-01-30 00:32:03 +0200

    adding items pink garnet and raw pink garnet

--- src/main/java/net/guy/tutorialmod/TutorialMod.java
+++ src/main/java/net/guy/tutorialmod/TutorialMod.java
@@ -2,6 +2,7 @@ package net.guy.tutorialmod;
 
 import net.fabricmc.api.ModInitializer;
 
+import net.guy.tutorialmod.item.ModItems;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
@@ -11,5 +12,6 @@ public class TutorialMod implements ModInitializer {
 
 	@Override
 	public void onInitialize() {
+		ModItems.registerModItems();
 	}
 }
